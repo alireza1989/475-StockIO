dfe1164a62d5a20ad565c2a4612633db31f2dc04
blng <blng@sfu.ca>
2017-03-25 02:27:09 -0700

    logo?

--- stockio/src/App.js
+++ stockio/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.svg';
+import logo1 from './images/logo1-white.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
         </div>
+
+        <nav className="App-navbar">
+          <div className="header-logo">
+            <h1>stockI</h1>
+            <img src={logo1} alt="logo"/>
+          </div>
+
+        </nav>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
       </div>
     );
   }
