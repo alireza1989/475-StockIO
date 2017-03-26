f5bc6406d327ec0ddf7bfc530ea1adcb22f1589f
adarbeha <adarbeha@sfu.ca>
2017-03-26 14:29:52 -0700

    fix merge conflict

--- web-app/db/models/Portfolio.js
+++ web-app/db/models/Portfolio.js
@@ -4,13 +4,18 @@ module.exports = function(sequelize, DataType) {
             type: DataType.STRING,
             field: 'name',
             allowNull: false
+        },
+        company: {
+            type: DataType.STRING,
+            field: 'company',
+            allowNull: false
         }
     }, {
         timestamps: false,
         classMethods: {
             associate: function(models) {
                 Portfolio.belongsToMany(models.User, { through: 'Users_Portfolios', timestamps: false});
-                Portfolio.belongsToMany(models.Company, {through: 'Companies_Portfolios', timestamps: false});         
+                Portfolio.belongsToMany(models.Company, {through: 'Companies_Portfolios', timestamps: false});
             }
         }
     });
