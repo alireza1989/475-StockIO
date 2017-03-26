2aa95056c092b4b6ba5332eb6a54e2c8626ce059
Sarah Sterchele <ssterche@sfu.ca>
2017-03-26 01:57:10 -0700

    move database files to db folder

---
+++ web-app/db/models/Portfolio.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataType) {
+    var Portfolio = sequelize.define('Portfolio', {
+        name: {
+            type: DataType.STRING,
+            field: 'name',
+            allowNull: false
+        }
+    }, {
+        timestamps: false,
+        classMethods: {
+            associate: function(models) {
+                Portfolio.belongsToMany(models.User, { through: 'Users_Portfolios', timestamps: false});
+                Portfolio.belongsToMany(models.Company, {through: 'Companies_Portfolios', timestamps: false});         
+            }
+        }
+    });
+    return Portfolio;
+};
