ae976c1b10b8f98c98321b6b1b956b20d2753d7f
Elliot Barer <ebarer@d207-023-212-038.wireless.sfu.ca>
2017-03-30 17:58:56 -0700

    Merge

---
+++ web-app/client/src/components/Portfolio.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PortfolioCell from './PortfolioCell';
+import Client from './Client';
+import './Portfolio.css';
+
+class Portfolio extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            companies: []
+        };
+    }
+   
+    componentWillMount() {
+        Client.getCompanies((companiesList) => { 
+            const companies = companiesList.map(obj => obj); //Is this redundant? Check.
+            this.setState({companies});
+        });
+    }
+
+    render() {
+        return (
+            <section className="stock-list">
+                <h2 className="stock-list-header">
+                    {this.props.name}
+                    <span className="stock-list-edit">Edit</span>
+                </h2>
+                
+                <div className="stock-list-navigation">
+                    <span className="nav-button prev">&lsaquo;</span>
+                    <span className="nav-button next">&rsaquo;</span>
+                </div>
+                
+                <div className="stocks">
+                    <ul>{this.state.companies.map((company, i) => <PortfolioCell key={i} company={company}/>)}</ul>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Portfolio;
