912d5df0e86b6085c629942fca22a195fac79a29
Elliot Barer <ebarer@d207-023-212-038.wireless.sfu.ca>
2017-03-30 17:17:16 -0700

    Rename and correct whitespace issues.

--- web-app/client/src/routes.js
+++ web-app/client/src/routes.js
@@ -1,25 +1,17 @@
-// This file uses REACT rutes library which helps us to build
-// a single page app that mimics a multiple pages app
-
-
-// Import React and the dependencies we need to make react router work
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-// Import the different components that will represent the different pages
 import App from './components/app';
-import DashBoard from './pages/Dashboard';
+import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
-
 
 // Define our routes
 const routes = (
-  <Route path='/' component={App}>
-    <IndexRoute component={DashBoard} />
-    <Route path='dashboard' component={DashBoard} />
-    <Route path='login' component={Login} />
-    
-  </Route>
+    <Route path='/' component={App}>
+        <IndexRoute component={Dashboard} />
+        <Route path='login' component={Login} />
+        <Route path='dashboard' component={Dashboard} />
+    </Route>
 );
 
 
