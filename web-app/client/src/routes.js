011f9c332bc7ec04b4c0abbb93d82a0654840c53
Sarah Sterchele <ssterche@sfu.ca>
2017-03-31 18:03:28 -0700

    remove already logged in check

--- web-app/client/src/routes.js
+++ web-app/client/src/routes.js
@@ -9,35 +9,21 @@ import Client from './components/Client';
 function requireAuth(nextState, replace, callback) {
     Client.currentUser((user)=> {
         if (user === undefined) {
+            console.log('req auth');
             replace({
                 pathname: '/login',
                 state: { nextPathname: nextState.location.pathname }
             });
+            callback();
         }
-        callback();
     });
 }
-
-function isLoggedOn(nextState, replace, callback) {
-    Client.currentUser((user)=> {
-        if (user !== undefined) {
-            replace({
-                pathname: '/dashboard',
-                state: { nextPathname: nextState.location.pathname }
-            });
-        }
-        callback();
-    });
-}
-
-
-
 
 // Define application routes
 const routes = (
     <Route path='/' component={App}>
         <IndexRoute component={Dashboard} onEnter={requireAuth} />
-        <Route path='login' component={Login} onEnter={isLoggedOn} />
+        <Route path='login' component={Login}/>
         <Route path='dashboard' component={Dashboard} onEnter={requireAuth} />
     </Route>
 );
