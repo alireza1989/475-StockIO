ccaa422959cccd58bb4aa326f6578136298d5413
adarbeha <adarbeha@sfu.ca>
2017-04-08 11:17:02 -0700

    add the missing history code in app.js

--- web-app/client/src/components/StockSummaryChart.js
+++ web-app/client/src/components/StockSummaryChart.js
@@ -24,7 +24,6 @@ class StockSummaryChart extends Component {
             // This will throw an error if the parent is closed before it loads
             if (this.state.mounted) {
 				var historyObj = JSON.parse(history);
-				//console.log(historyObj);
                 const fiveDayHistory = historyObj.data.map(obj => obj);
                 this.setState({fiveDayHistory});
 				console.log(fiveDayHistory);
@@ -35,30 +34,43 @@ class StockSummaryChart extends Component {
             // This will throw an error if the parent is closed before it loads
             if (this.state.mounted) {
 				var historyObj = JSON.parse(history);
-				//console.log(historyObj);
                 const oneYearHistory = historyObj.data.map(obj => obj);
                 this.setState({oneYearHistory});
 				console.log(oneYearHistory);
             }
         });
 
-		//var dates = this.state.fiveDayHistory.map(obj => obj);
 		/*var chart =*/ C3.generate({
-		bindto: '#chart',
-		data: {
-		  columns: [
-		    ['data1', 30, 200, 100, 400, 150, 250],
-		  ],
-		  axes: {
-		    data2: 'y2' // ADD
-		  }
-		},
+		    data: {
+		        x: 'x',
+		//        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+		        columns: [
+		            ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+		//            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
+		            ['Stock Price', 30, 200, 100, 400, 150, 250],
+		            //['data2', 130, 340, 200, 500, 250, 350]
+		        ]
+		    },
+		    axis: {
+		        x: {
+		            type: 'timeseries',
+		            tick: {
+		                format: '%Y-%m-%d'
+		            }
+		        }
+		    }
 		});
 	}
 
 	render() {
 		return (
 			<div className="stock-property">
+				<select>
+				    <option value="five-day">5 Day</option>
+				    <option value="one-month">1 Month</option>
+				    <option value="three-month">3 Month</option>
+				    <option value="one-year">1 Year</option>
+				</select>
                 <div id="chart"></div>
 			</div>
 		);
