03f23910929202aab794b42c62d947603ee8a111
Sarah Sterchele <ssterche@sfu.ca>
2017-04-08 00:05:18 -0700

    revert

--- web-app/client/src/routes.js
+++ web-app/client/src/routes.js
@@ -18,23 +18,23 @@ function requireAuth(nextState, replace, callback) {
     });
 }
 
-function checkAuth(nextState, replace, callback) {
-    Client.currentUser((user)=> {
-        if (user !== undefined) {
-            replace({
-                pathname: '/dashboard',
-                state: { nextPathname: nextState.location.pathname }
-            });
-        }
-        callback();
-    });
-}
+// function checkAuth(nextState, replace, callback) {
+//     Client.currentUser((user)=> {
+//         if (user !== undefined) {
+//             replace({
+//                 pathname: '/dashboard',
+//                 state: { nextPathname: nextState.location.pathname }
+//             });
+//         }
+//         callback();
+//     });
+// }
 
 // Define application routes
 const routes = (
     <Route path='/' component={App}>
         <IndexRoute component={Dashboard} />
-        <Route path='login' component={Login} onEnter={checkAuth} />
+        <Route path='login' component={Login} />
         <Route path='dashboard' component={Dashboard} onEnter={requireAuth} />
     </Route>
 );
