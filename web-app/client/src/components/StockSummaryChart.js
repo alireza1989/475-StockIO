5ba6c42a7f9d21873d396afe9b437e3e23ac65af
Elliot Barer <ebarer@mac.com>
2017-04-08 20:37:42 -0700

    Modified graph to use static data and better library

--- web-app/client/src/components/StockSummaryChart.js
+++ web-app/client/src/components/StockSummaryChart.js
@@ -1,41 +1,33 @@
 import React, { Component } from 'react';
-//import D3 from 'd3';
-import c3 from 'c3';
-import './StockSummaryChart.css';
 import Client from './Client';
+import {Line} from 'react-chartjs-2';
+import './StockSummaryChart.css';
 
 class StockSummaryChart extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-            ticker: props.symbol,
-            data: [
-                ['x', '2017-04-03', '2017-04-04', '2017-04-05', '2017-04-06', '2017-04-07'],
-                ['data', 146.92, 145.5, 143.62, 143.74, 143.11]
-            ]
-        };
-	}
-	
-	componentDidMount() {
-        var chart = c3.generate({
-            bindto: '#chart',
-            legend: { show: false },
-            size: { width: 430 },
+            ticker: props.symbol,            
             data: {
-                x: 'x',
-                columns: [
-                    ['x', '2017-04-03', '2017-04-04', '2017-04-05', '2017-04-06', '2017-04-07'],
-                    ['data', 146.92, 145.5, 143.62, 143.74, 143.11]
+                labels: ['2017-04-03', '2017-04-04', '2017-04-05', '2017-04-06', '2017-04-07'],
+                datasets: [
+                    {
+                        label: 'Data',
+                        data: [146.92, 145.5, 143.62, 143.74, 143.11],
+                        lineTension: 0.1,
+                        borderColor: 'red',
+                        pointBorderColor: 'red',
+                        pointBackgroundColor: 'red',
+                        pointBorderWidth: 5,
+                        pointHoverRadius: 5,
+                        pointRadius: 2,
+                        pointHitRadius: 20,
+                    }
                 ]
-            },
-            axis: {
-		        x: {
-		            type: 'timeseries',
-		            tick: { format: '%m/%d' }
-		        }
-		    }
-        });
+            }
+        };
+        
 	}
 	
 	updateChart = () => {
@@ -48,7 +40,9 @@ class StockSummaryChart extends Component {
 	render() {    	
 		return (
 			<div className="stock-summary-chart">			    
-                <div id="chart"></div>
+                <div id="chart">
+                    <Line data={this.state.data} />
+                </div>
 
 			    <ul className="stock-summary-chart-options">
 			        <li className='active'>5 Days</li>
